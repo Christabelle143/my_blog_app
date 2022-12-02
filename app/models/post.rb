9997cb7b8f3fa244fa62f745bfda0c78177b5228
Christabelle143 <christabelleyouta143@gmail.com>
2022-12-02 08:48:07 +0100

    create custom models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :likes
     has_many :comments
-end
 
+    def update_post_counter
+        user.increment!(:postsCounter)
+    end
+    def most_recent_comments 
+        comments.order(created_at: :DESC).limit(5)
+    end
+end
