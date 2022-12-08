58416bb5d9f18c6bbe747952416e2e9dedd24fcc
Christabelle143 <christabelleyouta143@gmail.com>
2022-12-08 10:57:01 +0100

    Create Users_controller action in the controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,5 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-    # @users = User.all.order(created_at: :desc)
-  end
+  def index; end
 
-  def show
-    @user = User.find(params[:id])
-    @posts = @user.most_recent_posts
-  end
+  def show; end
 end
