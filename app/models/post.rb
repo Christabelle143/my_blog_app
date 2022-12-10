6893cfcfbede5ff13433bce206616876bf64d7e9
Christabelle143 <christabelleyouta143@gmail.com>
2022-12-10 17:41:33 +0100

    Create a new.html view to display the added input form

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,14 @@ class Post < ApplicationRecord
   has_many :comments
   validates :title, :text, presence: true
 
-  validates :title, presence: true,
-                    length: { maximum: 250, too_long: 'Title must not exceed 250 characters' }
-  validates_numericality_of :commentsCounter, only_integer: true, greater_than_or_equal: 0
-  validates_numericality_of :likesCounter, only_integer: true, greater_than_or_equal: 0
+  # validates :title, presence: true,
+  #                   length: { maximum: 250, too_long: 'Title must not exceed 250 characters' }
+  # validates_numericality_of :commentsCounter, only_integer: true, greater_than_or_equal: 0
+  # validates_numericality_of :likesCounter, only_integer: true, greater_than_or_equal: 0
+
+  after_save :update_postsCounter
+
+  private
 
   def update_post_counter
     user.increment!(:postsCounter)
